555346788d23991bb2b2ebce47a85002bf487009
songmya <songmya9@gmail.com>
2025-04-25 10:43:36 +0800

    优化

--- js/api.js
+++ js/api.js
@@ -365,10 +365,7 @@ export async function callTxt2ImgApi(prompt) {
 
         return imageUrl;
 
-    } catch (error) {
-        console.error('Error calling Txt2Img API:', error);
-        throw error; // 抛出错误以便调用者处理
-    }
+    
 }
 
 /**
